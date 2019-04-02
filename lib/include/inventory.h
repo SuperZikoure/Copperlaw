a8a0acd0fd60d967c666270689240f5bf19ff7de
Bellanger Nathan <bellanger.nathan@epitech.eu>
2019-04-02 17:03:12 +0200

    correct : inventory.h

--- lib/include/inventory.h
+++ lib/include/inventory.h
@@ -42,34 +42,38 @@ int get_id(int id, inventory_t *inv);
 
 #endif /*!INVENTORY */
 
-/* CREATE_INVENTORY               my_rpg Programmer's Manual               CREATE_INVENTORY */
-/*                                                                                          */
-/* NAME                                                                                     */
-/*     create_inventory - create a pointer to a new inventory structure filled with 0       */
-/*                                                                                          */
-/* RETURN VALUE                                                                             */
-/*     The function return a pointer to an inventory_t struct or NULL if an error has       */
-/*     occurred                                                                             */
-/* my_rpg                                   2019-04-01                     CREATE_INVENTORY */
+/* CREATE_INVENTORY       my_rpg Programmer's Manual       CREATE_INVENTORY */
+/*                                                                          */
+/* NAME                                                                     */
+/*     create_inventory - create a pointer to a new inventory structure     */
+/*     filled with 0                                                        */
+/*                                                                          */
+/* RETURN VALUE                                                             */
+/*     The function return a pointer to an inventory_t struct or NULL if    */
+/*     an error has occurred                                                */
+/*                                                                          */
+/* my_rpg                      2019-04-01                  CREATE_INVENTORY */
 
-/* ADD_ITEM                       my_rpg Programmer's Manual                       ADD_ITEM */
-/*                                                                                          */
-/* NAME                                                                                     */
-/*     add_item - add an item to the inventory struct or stack it                           */
-/*                                                                                          */
-/* RETURN VALUE                                                                             */
-/*     The function return 0 if the item is correctly stacked else the function return -1 if*/
-/*     there is no more places in the inventory (in this case the item will be placed in tmp*/
-/*     part of the struct) and -2 if the inventory size is negative                         */
-/* my_rpg                                 2019-04-01                               ADD_ITEM */
+/* ADD_ITEM                my_rpg Programmer's Manual              ADD_ITEM */
+/*                                                                          */
+/* NAME                                                                     */
+/*     add_item - add an item to the inventory struct or stack it           */
+/*                                                                          */
+/* RETURN VALUE                                                             */
+/*     The function return 0 if the item is correctly stacked else the      */
+/*     function return -1 if there is no more places in the inventory       */
+/*     (in this case the item will be placed in tmp part of the struct)     */
+/*     and -2 if the inventory size is negative                             */
+/* my_rpg                      2019-04-01                          ADD_ITEM */
 
-/* ADD_STACK                      my_rpg Programmer's Manual                      ADD_STACK */
-/*                                                                                          */
-/* NAME                                                                                     */
-/*     add_stack - add a stack of items to the inventory struct                             */
-/*                                                                                          */
-/* RETURN VALUE                                                                             */
-/*     The function return 0 if the stack is correctly set else the function return -1 if   */
-/*     there is no more places in the inventory (in this case the stack will be placed in   */
-/*     tmp part of the struct) and -2 if the inventory size is negative                     */
-/* my_rpg                                 2019-04-01                               ADD_ITEM */
+/* ADD_STACK               my_rpg Programmer's Manual             ADD_STACK */
+/*                                                                          */
+/* NAME                                                                     */
+/*     add_stack - add a stack of items to the inventory struct             */
+/*                                                                          */
+/* RETURN VALUE                                                             */
+/*     The function return 0 if the stack is correctly set else the function*/
+/*     return -1 if there is no more places in the inventory (in this case  */
+/*     the stack will be placed in tmp part of the struct) and -2 if the    */
+/*     inventory size is negative                                           */
+/* my_rpg                      2019-04-01                          ADD_ITEM */

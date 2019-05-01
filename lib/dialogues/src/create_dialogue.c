4a99a789cd2cc2cfc0e337595eac11b0192a7441
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-01 18:42:24 +0200

    Dialogues are rescaled so that that arent like patés anymore on the view

--- lib/dialogues/src/create_dialogue.c
+++ lib/dialogues/src/create_dialogue.c
@@ -29,7 +29,7 @@ struct dialogue_s *create_dialogue(char *script, author_t author,
     dialogue->text = quick_text_create(30, dialogue_font_path);
     if (!dialogue->text)
         return NULL;
-    sfText_setPosition(dialogue->text, V2F(0, 0));
+    sfText_setScale(dialogue->text, V2F(.5, .5));
     sfText_setString(dialogue->text, dialogue->said);
     dialogue->choises = choises;
     dialogue->next = NULL;

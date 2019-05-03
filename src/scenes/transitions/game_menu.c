2d7cbcd0b244a536783f0b880996413c25c527d5
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-03 19:40:44 +0200

    Changed transition functions

--- src/scenes/transitions/game_menu.c
+++ src/scenes/transitions/game_menu.c
@@ -7,14 +7,14 @@
 
 #include "my_rpg.h"
 
-int swap_game_to_menu(game_t *game, int index __attribute__((unused)))
+int swap_game_to_menu(game_t *game, int option __attribute__((unused)))
 {
     change_scene(MENU);
     CURSOR->mode = CURSOR_BASE;
     return 1;
 }
 
-int swap_menu_to_game(game_t *game, int index __attribute__((unused)))
+int swap_menu_to_game(game_t *game, int option __attribute__((unused)))
 {
     change_scene(GAME);
     CURSOR->mode = CURSOR_IDLE;

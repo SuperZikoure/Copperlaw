bfa77928cb593f290b98a903e8ec032230991285
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-03 19:41:56 +0200

    Cleaned main_menu_scene

--- src/scenes/main_menu/main_menu_scene.c
+++ src/scenes/main_menu/main_menu_scene.c
@@ -10,13 +10,6 @@
 #include <math.h>
 #include <stdio.h>
 
-static int analyse_main_menu_events(game_t *game, input_t *input)
-{
-    if (KEY_PRESSED(ESCAPE_KEY))
-        return swap_main_menu_to_game(game, 0);
-    return 0;
-}
-
 static void display_menu(game_t *game)
 {
     static float pos = 0;
@@ -30,13 +23,13 @@ static void display_menu(game_t *game)
     test = (sfColor){ABS(color), ABS(color), ABS(color), 255};
     sfSprite_setColor(get_image(MENU_BG)->sprite, test);
     sfSprite_setColor(get_anim(MENU_PLAYER)->sheet->sprite, test);
-    display_image(get_image(MENU_SKY), global_to_view(V2F(pos, 0),VIEW));
+    display_image(get_image(MENU_SKY), GTV(pos, 0));
     pos += 2880;
-    display_image(get_image(MENU_SKY), global_to_view(V2F(pos, 0), VIEW));
+    display_image(get_image(MENU_SKY), GTV(pos, 0));
     pos -= 2880;
-    display_image(get_image(MENU_BG), global_to_view(V2F(0, 0), VIEW));
-    display_anim(get_anim(MENU_PLAYER), global_to_view(V2F(450, 250), VIEW));
-    display_image(get_image(MENU_LOGO), global_to_view(V2F(0, 0), VIEW));
+    display_image(get_image(MENU_BG), GTV(0, 0));
+    display_anim(get_anim(MENU_PLAYER), GTV(450, 250));
+    display_image(get_image(MENU_LOGO), GTV(0, 0));
     pos -= get_delta() / 2.0;
     if (pos <= -2880)
         pos = 0;
@@ -44,8 +37,6 @@ static void display_menu(game_t *game)
 
 int main_menu_scene(game_t *game)
 {
-    if (analyse_main_menu_events(game, game->input))
-        return 1;
     display_menu(game);
     show_scene_buttons(game);
     display_cursor(game);

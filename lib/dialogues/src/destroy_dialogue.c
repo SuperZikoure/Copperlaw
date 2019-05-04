792b78ac1da95be44d688c67eebf45309849ec4c
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-05 01:05:08 +0200

    Dialogues are well destroyed now

--- lib/dialogues/src/destroy_dialogue.c
+++ lib/dialogues/src/destroy_dialogue.c
@@ -10,11 +10,34 @@
 #include "dialogues.h"
 #include "macros.h"
 
-void destroy_dialogue(struct dialogue_s *dialogue)
+static void destroy_node(struct dialogue_s *dialogue)
 {
     free(dialogue->script);
     free(dialogue->said);
     DESTROY_TEXT_FONT(dialogue->text);
     sfText_destroy(dialogue->text);
     free(dialogue);
+}
+
+static void destroy_choises(struct selection *choises)
+{
+    for (int i = 0; i < choises->total; i++) {
+        DESTROY_TEXT_FONT(choises->texts[i]);
+        sfText_destroy(choises->texts[i]);
+    }
+    free(choises->texts);
+    free(choises);
+}
+
+void destroy_dialogue(struct dialogue_s *dialogue)
+{
+    struct dialogue_s *head = dialogue;
+
+    while (head) {
+        head = head->next;
+        if (dialogue->choises)
+            destroy_choises(dialogue->choises);
+        destroy_node(dialogue);
+        dialogue = head;
+    }
 }

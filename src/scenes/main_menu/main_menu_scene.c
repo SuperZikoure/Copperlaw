c469e9050e1fc41a8e8eb36320894ac803663970
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-04 21:01:15 +0200

    Added small intro cutscene

--- src/scenes/main_menu/main_menu_scene.c
+++ src/scenes/main_menu/main_menu_scene.c
@@ -7,8 +7,35 @@
 
 #include "my_rpg.h"
 #include "macros.h"
-#include <math.h>
-#include <stdio.h>
+
+static sfText *init_skip_text(game_t *game)
+{
+    sfText *text = quick_text_create(30, "assets/dialogues/font.ttf");
+    if (!text)
+        return NULL;
+    sfText_setScale(text, V2F(.5, .5));
+    sfText_setPosition(text, GTV(560, 380));
+    sfText_setString(text, "Click to skip...");
+    return text;
+}
+
+static int change_square_alpha(void)
+{
+    static unsigned char color = 255;
+    static size_t frames = 0;
+
+    sfSprite_setColor(get_image(BIG_SQUARE)->sprite, COLOR(0, 0, 0, color));
+    frames += get_delta();
+    if (frames < 6)
+        return 0;
+    frames -= 6;
+    color -= 1 * get_delta();
+    if (color <= 1 * get_delta()) {
+        color = 0;
+        return 1;
+    }
+    return 0;
+}
 
 static void display_menu(game_t *game)
 {
@@ -34,10 +61,36 @@ static void display_menu(game_t *game)
         pos = 0;
 }
 
+static bool display_intro(game_t *game, sfText *text)
+{
+    bool is_ended = false;
+    
+    if (change_square_alpha())
+        is_ended = true;
+    display_image(get_image(BIG_SQUARE), GTV(0, 0));
+    sfRenderWindow_drawText(WINDOW->window, text, NULL);
+    return !is_ended;
+}
+
 int main_menu_scene(game_t *game)
 {
+    static bool intro = true;
+    static sfText *text = NULL;
+
+    if (!text) {
+        text = init_skip_text(game);
+        if (!text)
+            return -1;
+    }
     display_menu(game);
-    show_scene_buttons(game);
+    if (MOUSE_PRESSED(sfMouseLeft))
+        intro = false;
+    if (intro) {
+        intro = display_intro(game, text);
+    } else {
+        display_image(get_image(MENU_LOGO), GTV(0, 0));
+        show_scene_buttons(game);
+    }
     display_cursor(game);
     return 0;
 }

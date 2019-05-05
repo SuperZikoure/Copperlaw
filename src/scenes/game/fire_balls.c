aaee818111cf3c3dad5d6692d771c211adf678ff
Ghassane SEBAÏ <ghassane.sebai@epitech.eu>
2019-05-05 21:54:25 +0200

    Fires ball

--- src/scenes/game/fire_balls.c
+++ src/scenes/game/fire_balls.c
@@ -16,32 +16,33 @@
 
 #include "my_rpg.h"
 
-/*void display_balls(ball_t *balls[PLAYER_BALLS])
+void display_balls(ball_t *balls[PLAYER_BALLS])
 {
     for (int i = 0; i < PLAYER_BALLS; i++) {
         if (!balls[i]->exist)
             continue;
         balls[i]->pos.x += balls[i]->speed.x;
         balls[i]->pos.y += balls[i]->speed.y;
-        display_image(balls[i]->ball, balls[i]->pos);
+        //display_image(balls[i]->ball, balls[i]->pos);
     }
 }
 
 void fire_ball(game_t *game, sfVector2f dir, int type, int skill)
 {
-    int x = pos.x + 16;
-    int y = pos.y + 16;
+    int x = PLAYER->pos.x + 16;
+    int y = PLAYER->pos.y + 16;
 
     for (int i = 0; i < PLAYER_BALLS; i++) {
         if (PLAYER->balls[i]->exist)
             continue;
-        balls[i]->exist = 1;
-        balls[i]->pos.x = x;
-        balls[i]->pos.y = y;
-        balls[i]->speed.x = dir.x * game->player->stats[ATTACK_SPEED];
-        balls[i]->speed.y = dir.y * game->player->stats[ATTACK_SPEED];
-        balls[i]->damage = PLAYER->stats[DAMAGE];
-        balls[i]->type;
+        PLAYER->balls[i]->exist = 1;
+        PLAYER->balls[i]->pos.x = x;
+        PLAYER->balls[i]->pos.y = y;
+        PLAYER->balls[i]->speed.x = dir.x * game->player->stats[ATTACK_SPEED];
+        PLAYER->balls[i]->speed.y = dir.y * game->player->stats[ATTACK_SPEED];
+        PLAYER->balls[i]->damage = PLAYER->stats[DAMAGE];
+        PLAYER->balls[i]->type = type;
+        PLAYER->balls[i]->skill = skill;
         return;
     }
-}*/
+}

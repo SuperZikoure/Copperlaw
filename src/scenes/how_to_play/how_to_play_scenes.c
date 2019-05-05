dcfae1901b7445cbc36ecf6dc8f70cf50106cc30
Bellanger Nathan <bellanger.nathan@epitech.eu>
2019-05-05 22:19:18 +0200

    added how_to_play

--- src/scenes/how_to_play/how_to_play_scenes.c
+++ src/scenes/how_to_play/how_to_play_scenes.c
@@ -8,12 +8,13 @@
 #include "my_rpg.h"
 #include "macros.h"
 
-static char const *msg = "[Z] to move up\n[S] to move down\n[Q] to move left\n[D] to move right\n[Space] to dash\n[Click] to interact with people\n[F11] to take a screenshot";
+static char const *board_path = "assets/scenes/how_to_play/board.png";
+static char const *msg = "[Z] to move up\n[S] to move down\n[Q] to move left\n[D] to move right[SPACE] to dash\n[F11] to make a screenshot"
 
 static int analyse_main_menu_events(game_t *game, input_t *input)
 {
     if (KEY_PRESSED(ESCAPE_KEY))
-        return swap_howtoplay_to_main_menu(game, 0);
+        return swap_option_to_main_menu(game, 0);
     return 0;
 }
 
@@ -22,9 +23,8 @@ int how_to_play_scene(game_t *game)
     if (analyse_main_menu_events(game, game->input))
         return 1;
     display_parallax(game);
-    display_image(get_image(HOWTOPLAY_IMG), GTV(0, 0));
-    display_text(msg, V2F(-200, -150),
-game->texts[BIG_TEXT]);
+    display_image(get_image(board_path), GTV(0, 0));
+    display_text(msg, V2F(50, 50));
     show_scene_buttons(game);
     display_cursor(game);
     return 0;
